3fc0e6a237f504365ca7daac8c6842792723a133
amirtotem <84120963+amirtotem@users.noreply.github.com>
2022-06-04 11:30:16 +0900

    Update nft-bank.ts

--- src/nft-bank.ts
+++ src/nft-bank.ts
@@ -3,6 +3,8 @@ import { PrivateModeIsOn } from "../generated/templates/NFTBank/NFTBank";
 import { NFTBankEntity } from "../generated/schema";
 
 export function handlePrivateModeIsOn(event: PrivateModeIsOn): void {
+	log.error("TEST_ERR", [event.address.toHexString()]);
+	
 	let entity = NFTBankEntity.load(event.address.toHexString());
 
 	if (entity == null) {
